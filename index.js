bd9105e05a562afeaa9b9b772225a43776b16690
Melany Yeung <mwyeung21@gmail.com>
2018-06-20 18:27:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Write your code in this file!
 
+// currentUser
+const currentUser = "Melany"
+
+// welcomeMessage
+const welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
+
+//excitedWelcomeMessage
+let excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + currentUser.toUpperCase() + "!"
+
+//shortGreeting
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
